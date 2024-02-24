e4d862e13158884dc05eb3a06fcae227a7527d0e
Kevin Sony <kevinsony2003@gmail.com>
2024-02-24 16:33:46 -0600

    assignment one: necessary data structures for this assignment

---
+++ Assignment 1/pipes_process_sync.c
@@ -0,0 +1,78 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_input_var(char* input_line, int* array, int* size) {
+	// initialize input_variables
+	for (int i = 0; i < 10; i++) {
+		array[i] = 0;
+	}
+	printf("%s\n", input_line);
+}
+
+void init_internal_var(char* internal_line, int* array, int* size) {
+	// initialize internal_variables
+	for (int i = 0; i < 10; i++) {
+		array[i] = 0;
+	}
+	printf("%s\n", internal_line);
+}
+
+/*
+*	This program takes in two arguments from terminal given by the user.
+*	
+*	The first argument is the file to precedence graph, and the second argument
+*	is the file to the inputs of the precedence graph.
+*
+*	The program outputs a file which contains the input variable and the 
+*	internal variables after it has gone through the precedence graph.
+*
+*	The way this program handles the code is to paralli
+*
+*/
+int main(int argc, char** argv) {
+	/*
+	*   Store the input variables and internal variables in an
+	*	pointer to an array. Store the number of input and internal
+	*	variables in a seperate data type.
+	*/
+	int* input_var;
+	int* internal_var;
+	int n_input_var;
+	int n_internal_var;
+
+	/*
+	*	Read the first two lines of the file for the precedence graph
+	*	to determine the number of input variables as well as internal variables.
+	*/
+	FILE* prec_graph = fopen(argv[1], "r");
+	FILE* input = fopen(argv[2], "r");
+
+	if (prec_graph == NULL || input == NULL) {
+		printf("The files are null\n");
+		exit(0);
+	}
+
+	char* line = NULL;
+	size_t len = 0;
+	printf("hello");
+	getline(&line, &len, prec_graph);
+	printf("hello");
+	init_input_var(line, input_var, &n_input_var);
+	printf("world");
+	line = NULL;
+	len = 0;
+	getline(&line, &len, prec_graph);
+	init_internal_var(line, internal_var, &n_internal_var);
+
+	/*
+	*	With the input variables and internal variables defined,
+	*	create a semaphore and fork the different processes.
+	*/
+	int sem = semget(2077318, 1, 0666 | IPC_CREAT);
+
+	return 0;
+}
