fd62d8c5ca1c8c37da0d27edaf9f63b238e77b4f
kevin sony <kevinsony2003@gmail.com>
2024-02-25 20:48:23 -0600

    assignment one: fixed seg issue caused by using null pointers

--- Assignment 1/non_mult_proc.c
+++ Assignment 1/non_mult_proc.c
@@ -1,3 +1,8 @@
+/*
+*	This program is the single process version of the precedence flow. It does not
+*	use semaphores or pipes to different processes, and the sole intention is to provide
+*	the framework on which the multiprocessor program can synchronize off of.
+*/
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -5,17 +10,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+*	This method takes in a string from the precedence graph line and initializes
+*	input variables. It does not fill in these variables initially, but does it later.
+*/
 void init_input_var(char* input_line, int* array, int* size) {
+	// Find the number of input variables given the input_line.
+	// This is done by finding the number of commas splitting the
+	// variables apart and adding 1 to it.
+	int n_elements = 1;
+	for (int i = 0; input_line[i] != '\0'; i++) {
+		if (input_line[i] == ',') n_elements++;
+	}
+	*size = n_elements;
+
+	array = (int *)malloc(sizeof(int) * n_elements);
 	// initialize input_variables
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < n_elements; i++) {
 		array[i] = 0;
 	}
 	printf("%s\n", input_line);
 }
 
 void init_internal_var(char* internal_line, int* array, int* size) {
-	// initialize internal_variables
-	for (int i = 0; i < 10; i++) {
+	// Find the number of internal variables given internal_line.
+	// This is done by finding the number of commas splitting the
+	// variables apart and adding 1 to it.
+	int n_elements = 1;
+	for (int i = 0; internal_line[i] != '\0'; i++) {
+		if (internal_line[i] == ',') n_elements++;
+	}
+	*size = n_elements;
+
+	array = (int *)malloc(sizeof(int) * n_elements);
+	// initialize input_variables
+	for (int i = 0; i < n_elements; i++) {
 		array[i] = 0;
 	}
 	printf("%s\n", internal_line);
@@ -39,7 +68,6 @@ int main(int argc, char** argv) {
 	*	pointer to an array. Store the number of input and internal
 	*	variables in a seperate data type.
 	*/
-    printf("hello");
 	int* input_var;
 	int* internal_var;
 	int n_input_var;
@@ -60,11 +88,8 @@ int main(int argc, char** argv) {
     
 	char* line = NULL;
 	size_t len = 0;
-	printf("hello");
 	getline(&line, &len, prec_graph);
-	printf("hello");
 	init_input_var(line, input_var, &n_input_var);
-	printf("world");
 	line = NULL;
 	len = 0;
 	getline(&line, &len, prec_graph);
